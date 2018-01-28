8766b7e7c98d6fbb759219c54f0c339a31813059
Aly Alidina <aly_alidina@hotmail.com>
2018-01-28 17:53:49 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,9 +1,38 @@
+require 'pry'
 class FiguresController < ApplicationController
-  get '/' do
-
+  get '/figures' do
+    erb :'/figures/index'
   end
 
   get '/figures/new' do
     erb :'/figures/new'
   end
+
+  post '/figures' do
+
+    @figures = Figure.create(params[:figure])
+
+    if !params["title"]["name"].empty?
+      @figures.titles << Title.create(params["title"])
+    end
+
+    if !params["landmark"]["name"].empty?
+      @figures.landmarks << Landmark.create(params["landmark"])
+    end
+
+    @figures.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :'/landmarks'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmarks = Landmark.create(params[:landmark])
+
+    if !params["landmark"]["name"].empty?
+      @figures.landmarks << Landmark.create(name: params["name"])
+    end
+    binding.pry
+  end
 end
+
+
+# rspec ./spec/controllers/models/figures_controller_spec.rb --fail-fast
